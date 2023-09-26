b399a33afaaf58dd2ef5d8b9cc64dbf3689c1adf
Amith Bino <amith1062@gmail.com>
2023-09-26 22:47:22 +0530

    second lesson

---
+++ src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <nav>
+        <v-toolbar :elevation="6">
+            <v-toolbar-title class="text-uppercase grey--text">
+            <span class="font-weight-light">Todo</span>
+            <span class="font-weight-dark">Wave</span>
+        </v-toolbar-title>
+        <v-space></v-space>
+        <v-btn flat-color="grey">
+            <span>Sign out</span>
+            <v-icon right class="mdi mdi-exit-to-app"></v-icon>
+        </v-btn>
+
+       </v-toolbar>    
+    </nav>
+</template>
+
+<script>
+export default{
+    name:'NavBar',
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
     <div class="home">
         <h1>Home Page</h1>
     </div>
+    <v-btn variant="tonal" color="pink white-text hidden-md-and-down">Button</v-btn>
 </template>
 
 <script>

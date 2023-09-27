11177f2a8f0394c194c31e60a83ed192c199567a
Amith Bino <amith1062@gmail.com>
2023-09-27 18:11:58 +0530

    navigation list

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
     <nav>
         <v-toolbar :elevation="6">
-            <v-app-bar-nav-icon class="grey--text" @click="drawer=!drawer"> </v-app-bar-nav-icon>
-            <v-toolbar-title class="text-uppercase grey--text">
+            <v-app-bar-nav-icon class="text-grey" @click="drawer=!drawer"> </v-app-bar-nav-icon>
+            <v-toolbar-title class="text-uppercase">
             <span class="font-weight-light">Todo</span> 
-            <span class="font-weight-dark">Wave</span>
+            <span class="text-deep-purple-darken-4">Wave</span>
         </v-toolbar-title>
         <v-space></v-space>
         <v-btn flat-color="grey">
@@ -12,8 +12,16 @@
             <v-icon right class="mdi mdi-exit-to-app"></v-icon>
         </v-btn>
        </v-toolbar> 
-       <v-navigation-drawer app temporary v-model="drawer" class="bg-indigo">
-    </v-navigation-drawer>   
+       <v-navigation-drawer app temporary v-model="drawer" class="bg-deep-purple-darken-4">
+        <v-list>
+            <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+            <template v-slot:prepend>
+            <v-icon color="white">{{ link.icon }}</v-icon>
+            </template>
+            <v-list-item-title class="text-white">{{ link.text }}</v-list-item-title>
+        </v-list-item>
+        </v-list>
+        </v-navigation-drawer>   
     </nav>
 </template>
 
@@ -23,6 +31,11 @@ export default{
     data(){
         return{
             drawer: false,
+            links: [
+                { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+                { icon: 'mdi-account', text: 'Team', route: '/team' },
+            ]
         }
     }
 }

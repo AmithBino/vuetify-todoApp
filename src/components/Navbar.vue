4aff4768992d1e0c8341d96803d9f89fe279c56a
Amith Bino <amith1062@gmail.com>
2023-09-27 16:01:06 +0530

    navigation drawer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,9 @@
 <template>
     <nav>
         <v-toolbar :elevation="6">
+            <v-app-bar-nav-icon class="grey--text" @click="drawer=!drawer"> </v-app-bar-nav-icon>
             <v-toolbar-title class="text-uppercase grey--text">
-            <span class="font-weight-light">Todo</span>
+            <span class="font-weight-light">Todo</span> 
             <span class="font-weight-dark">Wave</span>
         </v-toolbar-title>
         <v-space></v-space>
@@ -10,13 +11,19 @@
             <span>Sign out</span>
             <v-icon right class="mdi mdi-exit-to-app"></v-icon>
         </v-btn>
-
-       </v-toolbar>    
+       </v-toolbar> 
+       <v-navigation-drawer app temporary v-model="drawer" class="bg-indigo">
+    </v-navigation-drawer>   
     </nav>
 </template>
 
 <script>
 export default{
     name:'NavBar',
+    data(){
+        return{
+            drawer: false,
+        }
+    }
 }
 </script>
